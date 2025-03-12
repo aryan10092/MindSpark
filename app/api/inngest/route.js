c002a4241f94cc5873132843968cb5bcbafef5f1
aryan10092 <aryan9090900@gmail.com>
2025-03-13 01:42:20 +0530

    Update route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -5,9 +5,12 @@ import { serve } from "inngest/next";
 // import { inngest } from "@/inngest/client";
 import { createuser, helloWorld,generatenotes } from "@/inngest/functions";
 
+export const runtime='edge'
 
 export const { GET, POST, PUT } = serve({
+  
   client: inngest,
+  streaming;'allow',
   functions: [
     helloWorld,
     createuser,

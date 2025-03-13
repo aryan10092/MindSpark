d5b8a347cf3195602a7c0efe774244b2bc54596b
aryan10092 <aryan9090900@gmail.com>
2025-03-13 14:44:34 +0530

    Update route.js

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,6 @@
 
 import { serve } from "inngest/next";
-  import { inngest } from "../../../inngest/client";
+import { inngest } from "@/inngest/client";
 // import { createuser, helloWorld } from "../../../inngest/functions";
 // import { inngest } from "@/inngest/client";
 import { createuser, helloWorld,generatenotes } from "@/inngest/functions";

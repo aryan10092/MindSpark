595cd8fbef357a8b0552bf052720521206e1babe
aryan10092 <aryan9090900@gmail.com>
2025-03-13 15:12:48 +0530

    Update provider.js

--- app/provider.js
+++ app/provider.js
@@ -17,22 +17,10 @@ function Provider({children}) {
     },[user])
 
   const newuser=async()=>{
-    // if (!user?.username) {
-    //     throw new Error("Full name is not available");
-    //   }
-
-// const result=await db.select().from(USER_TABLE)
-// .where(eq(USER_TABLE.email,user?.primaryEmailAddress?.emailAddress))
-  
-
+    
 // console.log(result)
 
-// if(result?.length==0){
 
-//    const res= await db.insert(USER_TABLE).values({
-//         name:user?.username,
-//         email:user?.primaryEmailAddress?.emailAddress
-//     }).returning({id:USER_TABLE.id})
 const resp=await axios.post('/api/create-user',{user:user})
 
 console.log(resp.data)

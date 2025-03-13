7330053adc03400630382fc9e3ca71f99e6b5578
aryan10092 <aryan9090900@gmail.com>
2025-03-13 18:16:00 +0530

    Update route.js

--- app/api/course/route.js
+++ app/api/course/route.js
@@ -3,7 +3,7 @@ import { STUDY_TABLE } from "@/configs/schema"
 import { desc, eq } from "drizzle-orm"
 import { Search } from "lucide-react"
 import { NextResponse } from "next/server"
-// import { URL } from "uuid/dist/cjs/v35"
+
 
 
 export async function POST(req) {
@@ -34,29 +34,3 @@ return NextResponse.json({result:course[0]})
 }
 
 
-// export async function GET(req) {
-//     const url = req.url;
-//     const searchParams = new URL(url).searchParams; // Correct way to get search params
-//     const courseId = searchParams.get('courseId');
-  
-//     if (!courseId) {
-//       return NextResponse.json({ error: 'Missing courseId' }, { status: 400 });
-//     }
-  
-//     try {
-//       const course = await db
-//         .select()
-//         .from(STUDY_TABLE)
-//         .where(eq(STUDY_TABLE.courseId, courseId));
-  
-//       return NextResponse.json({ result: course });
-//     } catch (error) {
-//       return NextResponse.json(
-//         { error: 'Database query failed', details: error.message },
-//         { status: 500 }
-//       );
-//     }
-//   }
-
-
-  //a0d91f32-830e-464b-87a9-07f1ff885ac0

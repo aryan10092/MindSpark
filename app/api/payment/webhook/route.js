ff15eac11e2692beb7f3dd6fad3ef7968067c4f9
aryan10092 <aryan9090900@gmail.com>
2025-03-13 18:14:29 +0530

    Update route.js

--- app/api/payment/webhook/route.js
+++ app/api/payment/webhook/route.js
@@ -10,11 +10,10 @@ export async function POST(req){
 
     let data;
     let eventType;
-    // Check if webhook signing is configured.
+   
     const webhookSecret = process.env.STRIPE_WEBHOOK_KEY;
     if (webhookSecret) {
-      // Retrieve the event by verifying the signature using the raw body and secret.
-      let event;
+         let event;
       let signature = req.headers["stripe-signature"];
   
       try {
@@ -27,21 +26,18 @@ export async function POST(req){
         console.log(`⚠️  Webhook signature verification failed.`);
         return res.sendStatus(400);
       }
-      // Extract the object from the event.
+     
       data = event.data;
       eventType = event.type;
     } else {
-      // Webhook signing is recommended, but if the secret is not configured in `config.js`,
-      // retrieve the event data directly from the request body.
+      
       data = req.body.data;
       eventType = req.body.type;
     }
   
     switch (eventType) {
       case 'checkout.session.completed':
-        // Payment is successful and the subscription is created.
-        // You should provision the subscription and save the customer ID to your database.
-         
+      
         const result =await db.update(USER_TABLE)
         .set({
             isMember:true
@@ -50,17 +46,12 @@ export async function POST(req){
 
         break;
       case 'invoice.paid':
-        // Continue to provision the subscription as payments continue to be made.
-        // Store the status in your database and check when a user accesses your service.
-        // This approach helps you avoid hitting rate limits.
-        break;
+      break;
       case 'invoice.payment_failed':
-        // The payment failed or the customer does not have a valid payment method.
-        // The subscription becomes past_due. Notify your customer and send them to the
-        // customer portal to update their payment information.
+       
         break;
       default:
-        // Unhandled event type
+      
     }
     return NextResponse.json({result:'success'})
 

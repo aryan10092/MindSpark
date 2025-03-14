e342f5fded0f84eca0160563affb09a49b7e242d
aryan10092 <aryan9090900@gmail.com>
2025-03-14 23:43:16 +0530

    Update page.jsx

--- app/dashboard/boost/page.jsx
+++ app/dashboard/boost/page.jsx
@@ -58,7 +58,7 @@ function boost() {
             
             <div className="border rounded-xl p-6 shadow-md">
                 
-            \    <h3 className="text-2xl font-bold mb-2">Free Plan</h3>
+                <h3 className="text-2xl font-bold mb-2">Free Plan</h3>
                 <div className="text-3xl font-bold mb-4">
                     $0 <span className="text-gray-500 text-lg">/month</span>
                 </div>

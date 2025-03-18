9a8a0a724616be6e79986a3cddd5211a990070ba
aryan10092 <aryan9090900@gmail.com>
2025-03-18 19:26:30 +0530

    Update page.jsx

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -100,7 +100,7 @@ function Create() {
       router.replace('/dashboard')
       setTimeout(() => {
         toast.success("Please refresh this page to start your journey");
-      }, 20000);
+      }, 26000);
       console.log(result)
 
     } catch (error) {
